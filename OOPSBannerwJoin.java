dd41a547ddc7cd7b830d5560edb4f3f69e8320f2
tandonrhythm07 <rt5947@srmist.edu.in>
2026-03-03 15:21:03 +0530

    Added to UC-2

---
+++ OOPSBannerwJoin.java
@@ -0,0 +1,20 @@
+public class OOPSBannerwJoin {
+    public static void main(String[] args) {
+
+        // O      O      P      S
+        System.out.println(String.join(" ",
+                " *** ", " *** ", " **** ", " ****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*    "));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", " **** ", " ****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*     ", "    *"));
+
+        System.out.println(String.join(" ",
+                " *** ", " *** ", "*     ", " ****"));
+    }
+}
